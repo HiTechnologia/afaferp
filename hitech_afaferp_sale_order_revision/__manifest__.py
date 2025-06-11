a1db630beb8d2db47e1dfe7f1a048eff5469c16c
HiTechnologia <info@hitechnologia.com>
2025-06-12 00:11:32 +0400

    HiTech AFAFERP Sale Order Revision With Base Revision
    Just Rename and removed the extra credits.

---
+++ hitech_afaferp_sale_order_revision/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "HiTech AFAFERP Sale order revisions",
+    "summary": "Keep track of revised quotations",
+    "version": "18.0.1.0.0",
+    "category": "Sale Management",
+    "author": "HiTechnologia",
+    "company": "HiTechnologia",
+    "license": "AGPL-3",
+    "depends": ["hitech_afaferp_base_revision", "sale_management"],
+    "data": ["view/sale_order.xml"],
+    "installable": True,
+    "post_init_hook": "populate_unrevisioned_name",
+}
