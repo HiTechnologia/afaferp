7e5b1f82575e796c35d85d6649de8004e0d8b963
HiTechnologia <info@hitechnologia.com>
2025-06-13 14:30:44 +0400

    HiTech AFAFERP Base Revision
    only renamed the User interface ( UI ) name.

---
+++ base_revision/tests/base_revision_tester.py
@@ -0,0 +1,29 @@
+from odoo import fields, models
+
+
+class BaseRevisionTester(models.Model):
+    _name = "base.revision.tester"
+    _description = "Base Revision Tester"
+    _inherit = ["base.revision"]
+
+    name = fields.Char(required=True)
+    state = fields.Selection(
+        selection=[
+            ("draft", "Draft"),
+            ("confirmed", "Confirmed"),
+            ("cancel", "Cancel"),
+        ],
+        default="draft",
+    )
+    current_revision_id = fields.Many2one(
+        comodel_name="base.revision.tester",
+    )
+    old_revision_ids = fields.One2many(
+        comodel_name="base.revision.tester",
+    )
+
+    def action_confirm(self):
+        self.write({"state": "confirmed"})
+
+    def action_cancel(self):
+        self.write({"state": "cancel"})
