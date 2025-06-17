f16a487aace1b7210d422e4f904cb7d36af440b6
HiTechnologia <info@hitechnologia.com>
2025-06-17 18:42:07 +0400

    HiTech AFAFERP base view inheritance
    this module is used for hitech afaferp sale usability module because hitech afaferp sale usability has the dependency of this module.

---
+++ hitech_afaferp_base_view_inheritance_extension/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": " HiTech AFAFERP Extended view inheritance",
+    "version": "18.0",
+    "development_status": "Mature",
+    "author": "HiTechnologia",
+    "license": "LGPL-3",
+    "category": "Hidden/Dependency",
+    "summary": "Adds more operators for view inheritance",
+    "depends": ["base"],
+    "external_dependencies": {"python": ["astor"]},
+    "demo": ["demo/ir_ui_view.xml"],
+}
