9072909fe63d45e41a2a2c782cf007dcca97841f
HiTechnologia <info@hitechnologia.com>
2025-06-23 17:32:18 +0400

    HiTech AfAFERP PLM web 3d sale
    HiTech AFAFERP PLM web 3d sale module is renamed in manifest.

---
+++ plm_web_3d_sale/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "HiTech AFAFERP PLM Web 3d Support Sale",
+    "version": "18.0.0.1",
+    "author": "HiTechnologia",
+    "category": "Manufacturing/Product Lifecycle Management (PLM)",
+    "sequence": 15,
+    "license": "AGPL-3",
+    "summary": "",
+    "images": [],
+    "depends": ["plm",
+                "website_sale"],
+    "data": [
+        # views
+        "views/product_image.xml",
+    ],
+    "qweb": [],
+    "demo": [],
+    "test": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
