5c9927621d72d715a6f51ddc8956c24f6012c7f4
HiTechnologia <info@hitechnologia.com>
2025-06-23 17:29:16 +0400

    HiTech AFAFERP PLM Workflow Action
    HiTech AFAFERP PLM Workflow Custom Action module is renamed in manifest.

---
+++ plm_workflow_custom_action/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "HiTech AFAFERP Workflow Custom Actions",
+    "version": "18.0.1.0.0",
+    "author": "HiTechnologia",
+    "category": "Manufacturing/Product Lifecycle Management (PLM)",
+    "sequence": 1,
+    "summary": "workflow custom actions.",
+    "depends": ["plm", "base_automation"],
+    "license": "AGPL-3",
+    "data": [
+        "security/security.xml",
+        "views/plm_automated_wf_action.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
---
+++ plm_workflow_custom_action/models/__init__.py
@@ -0,0 +1,3 @@
+from . import ir_attachment
+from . import product_template
+from . import plm_automated_wf_actions
