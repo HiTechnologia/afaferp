917d3080dbd57b3b92c9f407d3fcb9e17e97d083
HiTechnologia <info@hitechnologia.com>
2025-06-26 12:52:53 +0400

    HiTech AFAFERP plm report language helper
    HiTech AFAFERP plm report language helper module is renamed in manifest.

---
+++ plm_report_language_helper/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "HiTech AFAFERP Plm Report Language Helper",
+    "version": "18.0.1.0.0",
+    "author": "HiTechnologia",
+    "category": "Manufacturing/Product Lifecycle Management (PLM)",
+    "sequence": 15,
+    "summary": "Manage multilanguage PLM reports",
+    "license": "AGPL-3",
+    "depends": ["plm", "plm_spare"],
+    "data": [
+        "security/plm_security.xml",
+        "wizard/select_lang_wizard_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
