df4ef82d9cf9541e1e1a5462d688d53a711b0020
HiTechnologia <info@hitechnologia.com>
2025-06-26 12:32:29 +0400

    HiTech AFAFERP plm client customprocedure
    HiTech AFAFERP plm client customprocedure  module is renamed in manifest.

---
+++ plm_client_customprocedure/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "HiTech AFAFERP PLM Client Custom Procedure",
+    "version": "18.0.1.0.0",
+    "author": "Hitechnologia",
+    "category": "Manufacturing/Product Lifecycle Management (PLM)",
+    "sequence": 15,
+    "summary": "PLM Client Custom Procedure",
+    "license": "AGPL-3",
+    "depends": ["plm"],
+    "data": [
+        "views/res_users.xml",
+        "views/res_groups.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
