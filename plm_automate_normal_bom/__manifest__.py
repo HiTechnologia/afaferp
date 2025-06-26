32ddc41031751160cfd3a4bc58502bda7bb00b98
HiTechnologia <info@hitechnologia.com>
2025-06-26 12:15:24 +0400

    HiTech AFAFERP plm automate normal
    HiTech AFAFERP plm automate normal module is renamed in manifest.

---
+++ plm_automate_normal_bom/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "HiTech AFAFERP PLM Automate Normal Bom",
+    "version": "18.0.1.0.0",
+    "author": "HiTechnologia",
+    "category": "Manufacturing/Product Lifecycle Management (PLM)",
+    "sequence": 15,
+    "summary": "Allow to create normal boms if not exists and product are released.",
+    "license": "AGPL-3",
+    "depends": ["plm", "plm_engineering"],
+    "data": [
+        "data/ir_cron.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
---
+++ plm_automate_normal_bom/models/__init__.py
@@ -0,0 +1,2 @@
+from . import product_product_extension
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
