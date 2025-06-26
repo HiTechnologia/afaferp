19bb86f22de213fb5b6620cbc0489993bf8ecad3
HiTechnologia <info@hitechnologia.com>
2025-06-26 12:34:29 +0400

    HiTech AFAFERP plm compare bom
    HiTech AFAFERP plm compare bom module is renamed in manifest.

---
+++ plm_compare_bom/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "HiTech AFAFERP PLM Compare Bom",
+    "version": "18.0.1.0.1",
+    "author": "HiTechnologia",
+    "category": "Manufacturing/Product Lifecycle Management (PLM)",
+    "sequence": 15,
+    "summary": "Allow to compare two boms",
+    "license": "AGPL-3",
+    "depends": ["plm"],
+    "data": [
+        "security/plm_security.xml",
+        "wizard/compare_bom_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
