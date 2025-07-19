fde126b82c78726a077b686613954770c15192dc
HiTechnologia <info@hitechnologia.com>
2025-07-19 17:56:15 +0400

    hitech_afaf_crm
    hitech_afaf_crm module has been upgraded with new functionalities.

---
+++ hitech_afaf_crm/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'HT AFAF CRM',
+    'version': '1.0',
+    'category': 'Sales/CRM',
+    'summary': 'Adds custom fields to CRM leads',
+    'description': 'This module adds custom fields like WEEK REF#, SELECTED BY MNGT, DATE RECEIVED, etc. to CRM leads.',
+    'author': 'HiTech Afaf',
+    'depends': ['crm', 'project', 'sale_crm', 'sale_management', 'hitech_afaf_project'],
+    'data': [
+        "security/ir.model.access.csv",
+        "views/hitech_afaf_crm.xml",
+        "views/hitech_afaf_crm_hide_quick_create.xml",
+    ],
+    'installable': True,
+    'application': False,
+}
